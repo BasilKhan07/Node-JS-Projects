320aa7eed3004099bc5e1fbafafe946a291fff66
BasilKhan07 <basilkhan06@gmail.com>
2024-07-02 17:26:52 +0500

    Setting up Authentication - NodeJS Blogging Application with MongoDB

--- NodeJS Blogging Application with MondoDB/index.js
+++ NodeJS Blogging Application with MondoDB/index.js
@@ -1,8 +1,16 @@
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+const Blog = require("./models/blogs");
 
 const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/blogify")
@@ -15,11 +23,19 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")))
 
-app.get("/", (req, res) => {
-  res.render("home");
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
 });
 
 app.use("/user", userRoute);
+app.use("/blog", blogRoute);
 
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
--- NodeJS Blogging Application with MondoDB/routes/user.js
+++ NodeJS Blogging Application with MondoDB/routes/user.js
@@ -13,9 +13,18 @@ router.get("/signup", (req, res) => {
 
 router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.matchPassword(email, password);
-  console.log("User: ", user);
-  res.redirect("/");
+  try {
+    const token = await User.matchPasswordAndGenerateToken(email, password);
+    return res.cookie("token", token).redirect("/");
+  } catch (error) {
+    return res.render("signin", {
+      error: "Incorrect Email or Password.",
+    });
+  }
+});
+
+router.get("/logout", (req, res) => {
+  res.clearCookie("token").redirect("/");
 });
 
 router.post("/signup", async (req, res) => {
